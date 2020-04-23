bc93ed42ef02c5641e53f1d4ba2f79dac569c28e
Mehulcoder <mehul355180@gmail.com>
2020-04-23 23:43:24 +0530

    Added custom pipe

--- src/tasks/dto/pipes/task-status-validation.pipe.ts
+++ src/tasks/dto/pipes/task-status-validation.pipe.ts
@@ -1,7 +1,26 @@
-import { PipeTransform } from "@nestjs/common";
-
+import { PipeTransform, BadRequestException } from "@nestjs/common";
+import { TaskStatus } from "src/tasks/task.model";
+//To be added in controller
 export class TaskStatusValidationPipe implements PipeTransform{
-    transform(){
+    
+    readonly allowedStatuses = [
+        TaskStatus.DONE,
+        TaskStatus.IN_PROGRESS,
+        TaskStatus.OPEN,
+    ]
+
+    transform(value:any){
+        value = value.toUpperCase();
         
+        if (this.isStatusValid(value)) {
+            return value;
+        }
+        
+        throw new BadRequestException(`${value} is an invalid status!`);
+    }
+
+    private isStatusValid(status:any){
+        const idx = this.allowedStatuses.indexOf(status);
+        return idx !== -1;
     }
 }
